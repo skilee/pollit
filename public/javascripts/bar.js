39db5acfb8e582062903fcc740252a93d8fdd3a1
raunaqrox <sahebjot94@gmail.com>
2014-06-14 00:10:10 +0530

    small fixes

--- public/javascripts/bar.js
+++ public/javascripts/bar.js
@@ -1,41 +1,4 @@
 $(function(){
-	//var canvas=document.getElementById("myCanvas")
-	//var ctx=canvas.getContext("2d");
-	/*var upVal=1;
-	var downVal=1;
-	var up=$('#up');
-	var down=$('#down');
-	up.html('yes '+upVal);
-	down.html('no '+downVal);
-	var widthRect1;
-	var widthRect2;
-	var widthFull=400;
-	var ratio;
-	var total;
-	up.on('click',function(){
-		upVal+=1;
-		up.html('yes '+upVal);
-		ratio=upVal/downVal;
-		rect();
-	});
-	down.on('click',function(){
-		downVal+=1;
-		down.html('no '+downVal);
-		ratio=upVal/downVal;
-		rect();
-	});
-	function rect(){
-		total=upVal+downVal;
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle="#555555";
-		widthRect1=widthFull*(upVal/total);
-		ctx.fillRect(0,0,widthRect1,25);
-		widthRect2=widthFull*(downVal/total);
-		ctx.fillStyle="#eeeeee";
-		ctx.fillRect(widthRect1,0,widthFull,25)
-		//ctx.fillRect(0,0,upVal*10,25);
-	}
-*/
 	function Poll(id){
 		this.id=id;
 		this.up=$('#up_'+id);
@@ -54,13 +17,15 @@ $(function(){
 	}
 	Poll.prototype.draw=function(){
 		var ctx=this.ctx;
+		var colorUp="#bbbbbb";
+		var colorDown="#dddddd";
 		var total=this.upVal+this.downVal;
 		ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		ctx.fillStyle="#555555";
+		ctx.fillStyle=colorUp;
 		var widthRect1=widthFull*(this.upVal/total);
 		ctx.fillRect(0,0,widthRect1,25);
 		var widthRect2=widthFull*(this.downVal/total);
-		ctx.fillStyle="#eeeeee";
+		ctx.fillStyle=colorDown;
 		ctx.fillRect(widthRect1,0,widthFull,25)
 	}
 
@@ -72,11 +37,13 @@ $(function(){
 		polls[i].up.click(function(){
 			var i=$(this).attr('id').split('_')[1]; 
 			polls[i].upVote();
+			$('#upVal_'+i).text(polls[i].upVal);
 			polls[i].draw();
 		});
 		polls[i].down.click(function(){
 			var i=$(this).attr('id').split('_')[1]; 
 			polls[i].downVote();
+			$('#downVal_'+i).text(polls[i].downVal);
 			polls[i].draw();
 		});
 		polls[i].draw();
